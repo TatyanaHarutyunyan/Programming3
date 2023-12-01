153541422564355bd88c66aca7ab4834ac713549
KarlenGhazaryan <karlen.ghazaryan.g@tumo.org>
2023-12-01 19:17:28 +0400

    inhertance

--- GameOfLife /GameOfLife/frog.js
+++ GameOfLife /GameOfLife/frog.js
@@ -1,7 +1,6 @@
-class Frog {
+class Frog extends LivingCreature {
     constructor(x, y) {
-        this.x = x
-        this.y = y
+      super(x,y)
         this.energy = 35
         this.directions = [];
     }
--- GameOfLife /GameOfLife/rabbit.js
+++ GameOfLife /GameOfLife/rabbit.js
@@ -1,7 +1,6 @@
-class Rabbit {
+class Rabbit extends LivingCreature{
     constructor(x,y){
-        this.x = x
-        this.y = y 
+        super(x,y)
         this.energy = 30
         this.directions = []
 
